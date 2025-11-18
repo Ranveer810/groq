b02aff45cbad15f870f48eadb700ef13cd17bf62
Ranveer810 <ranveerbangra703@gmail.com>
2025-11-18 10:39:34 +0530

    Add TabBar to header component
    Updated header component to include TabBar and removed unnecessary comments.

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,9 @@
-// Hataya gaya: Vercel link, divider, Groq icon, Deploy button
+import { TabBar } from "./tabbar";
 
 export const Header = () => {
   return (
-    <div className="fixed right-0 left-0 w-full top-0 bg-white dark:bg-zinc-950">
-      {/* Top bar ka sara content hata diya */}
+    <div className="fixed right-0 left-0 w-full top-0 bg-white dark:bg-zinc-950 z-10">
+      <TabBar />
     </div>
   );
 };
